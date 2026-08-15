7254d5fa24456c09bfaa51714c007e64936fd764
Delightfilm <jhpodong000000@gmail.com>
2026-08-15 20:15:10 +0900

    feat: mount 1688 collector inside main seller OS discovery

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,20 @@ import { createRoot } from "react-dom/client";
 import "./styles.css";
 import SmartStoreSellerOS from "./SmartStoreSellerOS.jsx";
 import SourcingConsole from "./SourcingConsole.jsx";
+import DiscoveryCollectorBridge from "./DiscoveryCollectorBridge.jsx";
 
 const path = window.location.pathname.replace(/\/+$/, "") || "/";
 const isSourcing = path === "/sourcing-console" || path === "/sourcing-console.html";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {isSourcing ? <SourcingConsole /> : <SmartStoreSellerOS />}
+    {isSourcing ? (
+      <SourcingConsole />
+    ) : (
+      <>
+        <SmartStoreSellerOS />
+        <DiscoveryCollectorBridge />
+      </>
+    )}
   </React.StrictMode>
 );
